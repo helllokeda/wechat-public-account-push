ae6bed258d132a80940deea2fa8978a31ce0ee4a
helllokeda <117791783+helllokeda@users.noreply.github.com>
2022-11-21 21:45:21 +0800

    Update and rename config/template-config.cjs to 配置/template-config.cjs

--- 配置/template-config.cjs
+++ 配置/template-config.cjs
@@ -2,7 +2,7 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '亲爱的, 早上好',
+    title: '早上好',
     desc: `
       **{{date.DATA}}**
       
@@ -40,16 +40,6 @@ const TEMPLATE_CONFIG = [
     title: '推送完成提醒',
     desc: `
       服务器信息：{{post_time_zone.DATA}} {{post_time.DATA}}
-      
-      ---
-      
-      共推送 {{need_post_num.DATA}} 人
-      
-      成功: {{success_post_num.DATA}} | 失败: {{fail_post_num.DATA}}
-      
-      成功用户: {{success_post_ids.DATA}}
-      
-      失败用户: {{fail_post_ids.DATA}}
     `
   },
 ]
